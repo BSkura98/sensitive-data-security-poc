c92e70c05eca34e1003139ec877209af810127a9
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2023-01-26 22:16:06 +0100

    Add tsconfig.json and fix 502 error code

--- backend/api/handler.ts
+++ backend/api/handler.ts
@@ -1,6 +1,15 @@
-export async function hello(event) {
-  return {
-    message: 'Go Serverless v3! Your function executed successfully!',
-    input: event,
-  };
-}
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const hello = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+    return ({
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Go Serverless v3.0! Your function executed successfully!",
+                input: event,
+            },
+        ),
+    });
+};
